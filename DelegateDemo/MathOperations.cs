a15de455b4f7f32e245e3881a6d0c3e7088d194d
Vic <985695123@qq.com>
2018-10-23 11:25:43 +0800

    添加多路广播demo

--- DelegateDemo/MathOperations.cs
+++ DelegateDemo/MathOperations.cs
@@ -5,13 +5,22 @@ using System.Text;
 
 namespace DelegateDemo
 {
-    delegate double MathsOp(double x);
+    delegate void MathsOp(double x);
+    // delegate double MathsOp(double x);
     class MathOperations
     {
-        public static double MultipBy2(double value)
+        public static void MultipBy2(double value)
         {
-            return value * 2;
+            double result = value * 2;
+            Console.WriteLine("Multiplying by 2: {0} gives {1}", value, result);
+           // return 
 
         }
+        public static void Squre(double value)
+        {
+            double result = value * value;
+            Console.WriteLine("Squaring: {0} gives {1}", value, result);
+        }
+     
     }
 }
--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -13,9 +13,13 @@ namespace DelegateDemo
         {
             //委托静态方法，也可以委托实例方法
             MathsOp operations = new MathsOp(MathOperations.MultipBy2);
+            operations += new MathsOp(MathOperations.Squre);
             double x = 1.23;
-            double result = operations(x);
-            Console.WriteLine("{0} multply by 2 is {1}", x, result);
+            operations(x);
+            operations -= new MathsOp(MathOperations.MultipBy2);
+            operations(x);
+            //double result = operations(x);
+            //Console.WriteLine("{0} multply by 2 is {1}", x, result);
             Console.Read();
         }
     }
