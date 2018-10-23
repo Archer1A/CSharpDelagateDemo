65d3f28c43bf88854bcf04de03279eb7c55821de
Vic <985695123@qq.com>
2018-10-23 11:05:59 +0800

    委托demo

---
+++ DelegateDemo/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelegateDemo
+{
+    delegate double MathsOp(double x);
+    class MathOperations
+    {
+        public static double MultipBy2(double value)
+        {
+            return value * 2;
+
+        }
+    }
+}
---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelegateDemo
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //委托静态方法，也可以委托实例方法
+            MathsOp operations = new MathsOp(MathOperations.MultipBy2);
+            double x = 1.23;
+            double result = operations(x);
+            Console.WriteLine("{0} multply by 2 is {1}", x, result);
+            Console.Read();
+        }
+    }
+}
